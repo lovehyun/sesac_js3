9f126e0c1d8f7f91b5d1720e2d856a64d78dbe8e
Su-Hyun Park <lovehyun@hotmail.com>
2025-12-04 14:37:40 +0900

    [학습] CSV reader/writer

--- 2.NODE/4.LIB/2.EXT/3.csv/3.csvread-noheader.js
+++ 2.NODE/4.LIB/2.EXT/3.csv/3.csvread-noheader.js
@@ -4,10 +4,11 @@ const fs = require('fs'); // 내장 모듈 (builtin 모듈)
 
 const results = [];
 
-fs.createReadStream('data.csv')
-    // fs.createReadStream('data-with-header.csv')
-    // fs.createReadStream('data-with-noheader.csv')
-    .pipe(csv())
+fs.createReadStream('data-with-noheader.csv')
+// fs.createReadStream('data-with-header.csv')
+    .pipe(csv({
+        headers: ['이름', '나이', '성별', '생년월일'] // 내가 원하는 키를 등록
+    }))
     .on('data', (data) => results.push(data))
     .on('end', () => {
         console.log(results);
