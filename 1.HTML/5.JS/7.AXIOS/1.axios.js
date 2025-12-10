ec98f525f40a38358a0908cd59a27d0ba2531d00
Su-Hyun Park <lovehyun@hotmail.com>
2025-12-10 17:11:19 +0900

    [신규] FE-BE간에 CRUD 완성

---
+++ 1.HTML/5.JS/7.AXIOS/1.axios.js
@@ -0,0 +1,27 @@
+// 1. 원래 fetch 로 요청하기.
+
+// 즉시실행함수 IIFE, Immediately Invoked Function Expression 
+// (async () => {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+//     const data = await res.json();
+//     console.log(data);
+// })();
+
+async function fetch_main() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+    const data = await res.json();
+    console.log(data);
+}
+
+// fetch_main();
+
+// 2. axios 로 요청하기.
+// npm install axios
+const axios = require('axios');
+async function axios_main() {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
+    const data = res.data;
+    console.log(data);
+}
+
+axios_main();
