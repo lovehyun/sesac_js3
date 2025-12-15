195905a9eb6f03147a23e77ae7e99cb0370fcce2
Su-Hyun Park <lovehyun@hotmail.com>
2025-12-15 12:01:55 +0900

    [미니플젝] 투두앱 간단 완성 - 최소한의 코드로 구현되었으며, 각종 에러처리 등 추가되어야 함 - 그 외에도 불필요한 주석코드 등도 제거해야 좋은 코드임

--- 3.EXPRESS/9.MINIAPPS/3.todoapp/public/js/script_await.js
+++ 3.EXPRESS/9.MINIAPPS/3.todoapp/public/js/script_await.js
@@ -2,13 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM 로딩 완료');
 
     // 최초 시작하자마다 백엔드에 가지고 있는 목록 달라고 요청한다.
-    function getTodo() {
-        fetch('/api/todos') // 요청
-            .then(res => res.json()) // 받아온 응답을 json으로 변환
-            .then(data => { // data라는 자료 구조로 받아와서 JS 코드에서 처리
-                console.log(data);
-                renderTodos(data);
-            });
+    async function getTodo() {
+        const res = await fetch('/api/todos'); // 요청
+        const data = await res.json();
+
+        console.log(data);
+        renderTodos(data);
     }
 
     getTodo();
@@ -32,18 +31,25 @@ document.addEventListener('DOMContentLoaded', () => {
             result.appendChild(li);
 
             // 토글도 추가할 예정
-            li.addEventListener('click', () => {
-                fetch(`/api/todo/${todo.id}/completed`, { method: 'PUT' })
-                    .then(() => getTodo()); // 요청이 끝났으면? 화면 업데이트
-            })
+            li.addEventListener('click', async () => {
+                const res = await fetch(`/api/todo/${todo.id}/completed`, { method: 'PUT' })
+                const data = await res.json();
+                console.log(data);
+                if (data.success == true) {
+                    getTodo(); // 요청이 끝났으면? 화면 업데이트
+                } else {
+                    alert('해당 항목은 찾을수 없습니다.');
+                }
+            });
 
             // 삭제버튼도 추가할 예정
             const deleteBtn = document.createElement('button');
             deleteBtn.textContent = "삭제";
-            deleteBtn.addEventListener('click', (e) => {
+            deleteBtn.addEventListener('click', async (e) => {
                 e.stopPropagation(); // 위에 토글까지 이 이벤트가 전파되는것을 방지.
-                fetch(`/api/todo/${todo.id}`, { method: 'DELETE'})
-                    .then(() => getTodo());
+                const res = await fetch(`/api/todo/${todo.id}`, { method: 'DELETE'})
+                const data = res.json(); // 리턴값 체크 아래에서 해야함
+                getTodo();
             })
             li.appendChild(deleteBtn);
 
@@ -52,19 +58,19 @@ document.addEventListener('DOMContentLoaded', () => {
     
     // todo 추가
     const addBtn = document.getElementById('add-todo');
-    addBtn.addEventListener('click', () => {
+    addBtn.addEventListener('click', async () => {
         // 입력값 검증
         const inputText = document.getElementById('new-todo').value;
         const text = inputText.trim(); // 빈공백들 제거
         console.log(text);
         if (!text) return; // 빈칸이면 조용히 끝.. 안보냄!!
 
-        fetch('/api/todo', {
+        const res = await fetch('/api/todo', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({todo:text}) // 문자화 또는 직렬화(serialization) 한다.
-        }).then(data => {
-            getTodo();
-        });
+        })
+        const data = res.json();
+        getTodo();
     });
 });
